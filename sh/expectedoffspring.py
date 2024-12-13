65af818f02618e0ff2c078a638d9790068d6c0d4
Carter <caya1.carter1@gmail.com>
2024-12-12 21:49:50 -0600

    Finish expected offspring.

---
+++ sh/expectedoffspring.py
@@ -0,0 +1,15 @@
+nDD = 19380
+nDH = 18715
+nDR = 16665
+nHH = 16680
+nHR = 18320
+nRR = 18723
+
+offspring = 0
+offspring += nDD * 2
+offspring += nDH * 2
+offspring += nDR * 2
+offspring += nHH * 1.5
+offspring += nHR * 1
+
+print(offspring)
